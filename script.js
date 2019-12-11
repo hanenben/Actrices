aa0da0e5d844cf8c7dfe9615b16e1b669a8c0760
hanenben <hanendjebari@gmail.com>
2019-12-11 15:58:52 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+
+$(document).ready(function() {
+$(".btn1").click(function(){
+        $(".div1, .img12").toggle();
+        $(".img1").toggle();
+        });      
+
+});
+
+$(document).ready(function() {
+        $(".btn2").click(function(){
+                $(".div12, .img21").toggle();
+                $(".img2").toggle();
+                $("#block11").toggle();
+                });      
+        
+});
+// $(document).ready(function() {
+// 	$(".btn3").on("click",function(){
+//         $(".img31, .div13").css("display","inline");
+//         $(".img3").css("display","none");
+		
+// 	});
+// });
+// $(document).ready(function() {
+// 	$(".btn4").on("click",function(){
+//         $(".img41, .div14").css("display","inline");
+//         $(".img4").css("display","none");
+		
+// 	});
+// });
+// $(document).ready(function() {
+// 	$(".btn5").on("click",function(){
+//         $(".img51, .div15").css("display","inline");
+//         $(".img5").css("display","none");
+		
+// 	});
+// });
